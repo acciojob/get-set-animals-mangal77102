4e00b3118b9509c3a83696f665126d65720854df
mangal77102 <mangalsabu2003@gmail.com>
2026-02-12 14:38:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-//complete this code
-class Animal {
- constuctor (name)
-	{
-		this.name=name;
-	}
-	get name ()
-	{
-		return this.name;
-	}
+//completclass Animal {
+  constructor(name) {
+    this._name = name;
+  }
 
-	makesound()
-	{
-		console.log(`The ${this.name} make a sound`);
-	}
+  get name() {
+    return this._name;
+  }
+
+  makeSound() {   // ✅ fixed
+    console.log(`The ${this.name} makes a sound`);  // ✅ fixed
+  }
 }
 
 class Dog extends Animal {
-	constuctor(name)
-	{
-		super(name);
-	}
-	
-	bark()
-	{
-		console.log("woof");
-	}
-	
+  constructor(name) {
+    super(name);
+  }
+
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	constuctor(name)
-	{
-		super(name);
-	}
-	
-	purr()
-	{
-		console.log("purr");
-	}
-	
+  constructor(name) {
+    super(name);
+  }
+
+  purr() {
+    console.log("purr");
+  }
 }
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
